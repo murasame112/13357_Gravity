4310b5c6aa0db0ac51caa99d4adf1aa6ccbb5eb3
murasame112 <79984587+murasame112@users.noreply.github.com>
2021-05-18 14:11:58 +0200

    done

--- JsAnimation/script.js
+++ JsAnimation/script.js
@@ -1,19 +1,49 @@
 const box1 = document.querySelector('#b1');
 const box2 = document.querySelector('#b2');
 
+// function rownanie(){
+//     box1.style.zIndex=2;
+//     box2.style.zIndex=1;
+// }
 
 
 const btn = document.querySelector('#btn');
 btn.addEventListener('click',()=>{
 
-if((box1.className!='box front')&&(box1.className!='box back')){
+   
+        
+        
+        
+
+if((box1.className=='box')&&(box1.className=='box')){
 box1.classList.add('front');
 box2.classList.add('back');
 }else if(box1.className=='box front'){
     box1.classList.remove('front');
+    box1.classList.add('front_rev');
     box2.classList.remove('back');
+    box2.classList.add('back_rev');
+
+    setTimeout(function(){  box1.classList.remove('front_rev');
+    box1.classList.add('back_again');
+    box2.classList.remove('back_rev');
+    box2.classList.add('front_again'); }, 1001);
+    
+   
+}else if((box1.className=='box back_again')&&(box2.className=='box front_again')){
+    box1.classList.remove('back_again');
+    box1.classList.add('back_again_rev');
+    box2.classList.remove('front_again');
+    box2.classList.add('front_again_rev');
 }
+
+
+setTimeout(function(){ 
+    box1.classList.remove('back_again_rev');
+    box2.classList.remove('front_again_rev'); }, 1001);
+
 
 
 
 })
+
